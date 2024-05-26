73f7e563c6113ecc5fcdc4241643abfb61714f98
Kirilligu <149255706+Kirilligu@users.noreply.github.com>
2024-05-27 01:43:53 +0800

    Update dependencies.py

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,1 +1,11 @@
+from fastapi import HTTPException, Depends
+from sqlalchemy.orm import Session
+from .models import User, Dog, Collar, Task, SessionLocal
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
