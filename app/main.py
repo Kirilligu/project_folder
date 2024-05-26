1e1b7bb7be8399a0a1f0c7d75fc63572af19ffa9
Kirilligu <149255706+Kirilligu@users.noreply.github.com>
2024-05-26 23:24:10 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
+from fastapi import FastAPI
+from app.routers import auth, collar, dog, task
+
+app = FastAPI()
+
+app.include_router(auth.router, prefix="/auth", tags=["auth"])
+app.include_router(collar.router, prefix="/collar", tags=["collar"])
+app.include_router(dog.router, prefix="/dog", tags=["dog"])
+app.include_router(task.router, prefix="/task", tags=["task"])
 
