7c9e9f91afe86216c4d28971c0afb5301e954150
Kirilligu <149255706+Kirilligu@users.noreply.github.com>
2024-05-27 01:16:55 +0800

    Delete app directory

--- app/models/collar.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-class Collar(BaseModel):
-    id: int
-    collar_number: str
-    characteristics: str
-
--- app/models/dog.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-class Dog(BaseModel):
-    id: int
-    name: str
-    description: str
-
