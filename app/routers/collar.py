e4af89f2de5b11248bedf304be158a4cacde07a9
Kirilligu <149255706+Kirilligu@users.noreply.github.com>
2024-05-26 23:23:22 +0800

    Update collar.py

--- app/routers/collar.py
+++ app/routers/collar.py
@@ -1,1 +1,10 @@
 
+from fastapi import APIRouter
+from app.models.collar import Collar
+
+router = APIRouter()
+
+@router.post("/register")
+def register_collar(collar: Collar):
+    # Логика регистрации нового ошейника
+    pass
