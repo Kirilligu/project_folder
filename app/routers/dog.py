ae182c3dd7b8add1a4a9a848d50353461970974b
Kirilligu <149255706+Kirilligu@users.noreply.github.com>
2024-05-26 23:23:37 +0800

    Update dog.py

--- app/routers/dog.py
+++ app/routers/dog.py
@@ -1,1 +1,10 @@
 
+from fastapi import APIRouter
+from app.models.dog import Dog
+
+router = APIRouter()
+
+@router.post("/register")
+def register_dog(dog: Dog):
+    # Логика регистрации новой собаки
+    pass
