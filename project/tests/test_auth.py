7360b05ce91e0d25736c94d2ce932e5e55b7a518
Kirilligu <149255706+Kirilligu@users.noreply.github.com>
2024-05-27 01:22:22 +0800

    Update test_auth.py

--- project/tests/test_auth.py
+++ project/tests/test_auth.py
@@ -1,1 +1,7 @@
+from app.auth import verify_password
+
+def test_verify_password():
+    hashed_password = "$2b$12$CdF17lV4vTVFLf.V5C9f1Ob4vTwPUG69i1b3Q6YgLopv3fJx.bXp6" # Hashed password for "password"
+    assert verify_password("password", hashed_password) == True
+    assert verify_password("wrongpassword", hashed_password) == False
 
