adaba9fbd1016c2d27167a0a8faac66ab62d6d66
Kirilligu <149255706+Kirilligu@users.noreply.github.com>
2024-05-27 16:12:07 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,13 +1,15 @@
 from fastapi import FastAPI
-from .database import engine
-from . import models
-from .users import router as user_router
-from .devices import router as device_router
+from .database import engine, Base
+from .users.router import router as users_router
+from .devices.router import router as devices_router
+from .tasks.router import router as tasks_router
+
+# Создание базы данных
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-models.Base.metadata.create_all(bind=engine)
-
-app.include_router(user_router)
-app.include_router(device_router)
-
+# Регистрация роутеров
+app.include_router(users_router, prefix="/users", tags=["users"])
+app.include_router(devices_router, prefix="/devices", tags=["devices"])
+app.include_router(tasks_router, prefix="/tasks", tags=["tasks"])
