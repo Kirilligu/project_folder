6f87fe6fd30f4d85c4a6a421a29441e9867cfebb
Kirilligu <149255706+Kirilligu@users.noreply.github.com>
2024-05-27 16:14:20 +0800

    Update models.py

--- src/tasks/models.py
+++ src/tasks/models.py
@@ -1,1 +1,15 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from ..database import Base
+
+class Task(Base):
+    __tablename__ = "tasks"
+    id = Column(Integer, primary_key=True, index=True)
+    description = Column(String, index=True)
+    status = Column(String, index=True)
+    dog_id = Column(Integer, ForeignKey("dogs.id"))
+    user_id = Column(Integer, ForeignKey("users.id"))
+
+    dog = relationship("Dog", back_populates="tasks")
+    user = relationship("User", back_populates="tasks")
 
