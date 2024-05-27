5a1ffc7d104f61d4d653ae021e035153b868633f
Kirilligu <149255706+Kirilligu@users.noreply.github.com>
2024-05-27 16:15:47 +0800

    Create utils.py

---
+++ src/utils.py
@@ -0,0 +1,13 @@
+from passlib.context import CryptContext
+import secrets
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def get_password_hash(password: str) -> str:
+    return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+def create_api_key() -> str:
+    return secrets.token_hex(16)
