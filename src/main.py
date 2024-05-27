e3075418a89ce6cee29ac9a305578dbd7d7c18dd
Kirilligu <149255706+Kirilligu@users.noreply.github.com>
2024-05-27 15:51:39 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,13 @@
+from fastapi import FastAPI
+from .database import engine
+from . import models
+from .users import router as user_router
+from .devices import router as device_router
+
+app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+
+app.include_router(user_router)
+app.include_router(device_router)
 
