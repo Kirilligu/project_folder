f7fde871e67b858baa4358e33d14ce097c2a4c1f
Kirilligu <149255706+Kirilligu@users.noreply.github.com>
2024-05-27 15:50:43 +0800

    Update models.py

--- src/devices/models.py
+++ src/devices/models.py
@@ -1,1 +1,20 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from ..database import Base
+
+class Collar(Base):
+    __tablename__ = "collars"
+
+    id = Column(Integer, primary_key=True, index=True)
+    unique_number = Column(String, unique=True, index=True)
+
+class Dog(Base):
+    __tablename__ = "dogs"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    description = Column(String)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
+    owner = relationship("User", back_populates="dogs")
 
