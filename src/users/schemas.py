e3806f33e638f4af3613514b46a163f704c3bba9
Kirilligu <149255706+Kirilligu@users.noreply.github.com>
2024-05-27 15:50:09 +0800

    Update schemas.py

--- src/users/schemas.py
+++ src/users/schemas.py
@@ -1,1 +1,16 @@
+from pydantic import BaseModel
+
+class UserBase(BaseModel):
+    first_name: str
+    last_name: str
+    phone_number: str
+
+class UserCreate(UserBase):
+    password: str
+
+class User(UserBase):
+    id: int
+
+    class Config:
+        orm_mode = True
 
