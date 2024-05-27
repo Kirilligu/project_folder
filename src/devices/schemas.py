0a9563c790c0fd85329d3f04b513d8eeab9b1b39
Kirilligu <149255706+Kirilligu@users.noreply.github.com>
2024-05-27 15:50:54 +0800

    Update schemas.py

--- src/devices/schemas.py
+++ src/devices/schemas.py
@@ -1,1 +1,28 @@
+from pydantic import BaseModel
+
+class CollarBase(BaseModel):
+    unique_number: str
+
+class CollarCreate(CollarBase):
+    pass
+
+class Collar(CollarBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+class DogBase(BaseModel):
+    name: str
+    description: str
+
+class DogCreate(DogBase):
+    pass
+
+class Dog(DogBase):
+    id: int
+    owner_id: int
+
+    class Config:
+        orm_mode = True
 
