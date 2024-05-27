30fbb9f67ad5601c72c205b69885a7c71e75aa46
Kirilligu <149255706+Kirilligu@users.noreply.github.com>
2024-05-27 16:50:00 +0800

    Update models.py

--- src/users/models.py
+++ src/users/models.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 from ..database import Base
 
 class User(Base):
@@ -9,3 +10,5 @@ class User(Base):
     phone_number = Column(String, unique=True, index=True)
     hashed_password = Column(String)
     api_key = Column(String, unique=True, index=True)
+    
+    tasks = relationship("Task", back_populates="user")
