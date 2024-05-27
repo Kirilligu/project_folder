f0c6246eee3a1f33019edc1aad2f60fa8222ed79
Kirilligu <149255706+Kirilligu@users.noreply.github.com>
2024-05-27 15:49:56 +0800

    Update models.py

--- src/users/models.py
+++ src/users/models.py
@@ -1,1 +1,14 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from ..database import Base
 
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    phone_number = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+
+    dogs = relationship("Dog", back_populates="owner")
